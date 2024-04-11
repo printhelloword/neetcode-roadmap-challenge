dc258ab22d61ad7a4c252152d05af65bff330b36
printhelloword <slessudjono@gmail.com>
2024-04-11 11:33:59 +0700

    solve topKFrequent

--- src/arraysandhasing/TopKFrequentElement.java
+++ src/arraysandhasing/TopKFrequentElement.java
@@ -1,11 +1,12 @@
 package arraysandhasing;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class TopKFrequentElement {
     public static void main(String[] args) {
-        topKFrequent(new int[]{1, 1, 1, 1, 2, 2, 3, 3}, 2);
-        topKFrequent(new int[]{5, 4, 6, 8, 7, 2, 8, 3, 4, 6, 5, 3, 2, 1, 2, 3}, 2);
+        System.out.println(Arrays.toString(topKFrequent(new int[]{1, 1, 1, 1, 2, 2, 3, 3, 2, 2, 2}, 3)));
+        System.out.println(Arrays.toString(topKFrequent(new int[]{5, 4, 6, 8, 7, 2, 8, 3, 4, 6, 5, 6, 3, 6, 2, 6, 2, 3}, 2)));
     }
 
     public static int[] topKFrequent(int[] nums, int k) {
@@ -16,8 +17,19 @@ public class TopKFrequentElement {
             else
                 map.put(num, map.get(num) + 1);
         }
-        System.out.println(map);
 
-        return new int[]{1};
+        /* Sort Keys asc frequent */
+        int[] keys = map.keySet().stream().mapToInt(Integer::intValue).toArray();
+        for (int i = 0; i < keys.length; i++) {
+            for (int j = 0; j < keys.length - 1; j++) {
+                if (map.get(keys[j + 1]) > map.get(keys[j])) {
+                    int temp = keys[j];
+                    keys[j] = keys[j + 1];
+                    keys[j + 1] = temp;
+                }
+            }
+        }
+
+        return Arrays.stream(keys).limit(k).toArray();
     }
 }
