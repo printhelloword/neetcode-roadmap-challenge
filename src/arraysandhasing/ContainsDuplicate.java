54e95e51456c4ef5b304ad6742b4795b2b5177ac
printhelloword <slessudjono@gmail.com>
2024-04-11 11:40:03 +0700

    add current solution to contains duplicate

--- src/arraysandhasing/ContainsDuplicate.java
+++ src/arraysandhasing/ContainsDuplicate.java
@@ -1,4 +1,22 @@
 package arraysandhasing;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContainsDuplicate {
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{1, 2, 3, 1}));
+        System.out.println(containsDuplicate(new int[]{1, 2, 3, 4, 5, 6}));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (temp.contains(nums[i]))
+                return true;
+            else
+                temp.add(nums[i]);
+        }
+        return false;
+    }
 }
