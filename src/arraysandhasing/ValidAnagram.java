a565e9d57e94e102fabffdd5652d45e23571caa7
printhelloword <slessudjono@gmail.com>
2024-04-13 23:02:18 +0700

    init valid anagram cases

---
+++ src/arraysandhasing/ValidAnagram.java
@@ -0,0 +1,16 @@
+package arraysandhasing;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        System.out.println(isAnagram("anagram", "managra"));
+        System.out.println(isAnagram("loop", "pool"));
+        System.out.println(isAnagram("astro", "ortsi"));
+        System.out.println(isAnagram("dople", "dploy"));
+        System.out.println(isAnagram("sushi", "sishu"));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        return false;
+    }
+
+}
