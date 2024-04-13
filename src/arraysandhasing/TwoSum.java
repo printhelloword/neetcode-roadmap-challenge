863793d33180239888858ebae707b4db07232259
printhelloword <slessudjono@gmail.com>
2024-04-13 23:48:58 +0700

    solution to twoSums

--- src/arraysandhasing/TwoSum.java
+++ src/arraysandhasing/TwoSum.java
@@ -1,4 +1,27 @@
 package arraysandhasing;
 
+import java.util.Arrays;
+
 public class TwoSum {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3}, 3)));
+        System.out.println(Arrays.toString(twoSum(new int[]{1, 4, 8}, 12)));
+        System.out.println(Arrays.toString(twoSum(new int[]{1, 2, 3, 4, 8, 9, 5}, 6)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] twoSumIndices = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = nums.length - 1; j > 0; j--) {
+                if ((nums[i] + nums[j]) == target) {
+                    twoSumIndices[0] = i;
+                    twoSumIndices[1] = j;
+                    return twoSumIndices;
+                }
+            }
+        }
+        return null;
+    }
 }
