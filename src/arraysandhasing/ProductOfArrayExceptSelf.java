69c96e09c66edb78089dff32b6bd74aaadad7dd9
printhelloword <slessudjono@gmail.com>
2024-05-08 17:40:31 +0700

    add solution to with time complexity of O(n)

--- src/arraysandhasing/ProductOfArrayExceptSelf.java
+++ src/arraysandhasing/ProductOfArrayExceptSelf.java
@@ -8,6 +8,11 @@ public class ProductOfArrayExceptSelf {
     public static void main(String[] args) {
         System.out.println(Arrays.toString(productExceptSelf(new int[]{1, 2, 3, 4})));
         System.out.println(Arrays.toString(productExceptSelf(new int[]{-1, 1, 0, -3, 3})));
+
+        System.out.println();
+
+        System.out.println(Arrays.toString(productExceptSelfUsingPrefixPostfix(new int[]{1, 2, 3, 4})));
+        System.out.println(Arrays.toString(productExceptSelfUsingPrefixPostfix(new int[]{-1, 1, 0, -3, 3})));
     }
 
     public static int[] productExceptSelf(int[] nums) {
@@ -26,6 +31,41 @@ public class ProductOfArrayExceptSelf {
         for (int i = 0; i < nums.length; i++) {
             if (i == index) continue;
             result *= nums[i];
+        }
+        return result;
+    }
+
+    public static int[] productExceptSelfUsingPrefixPostfix(int[] nums) {
+        int[] prefix  = new int[nums.length];
+        int[] postfix = new int[nums.length];
+        prefix[0]=1;
+        postfix[nums.length-1]=1;
+        int startPrefixPostfix = 1;
+
+        for(int i=0; i < nums.length ; i++){
+            if (i==0){
+                prefix[i] = nums[i] * startPrefixPostfix;
+            }else
+                prefix[i] = nums[i] * prefix[i-1];
+        }
+        System.out.println("prefixes : " + Arrays.toString(prefix));
+
+        for(int i=nums.length-1; i >= 0 ; i--){
+            if (i==nums.length-1)
+                postfix[i] = nums[i] * startPrefixPostfix;
+            else
+                postfix[i] = nums[i] * postfix[i+1];
+        }
+        System.out.println("postfixes : " + Arrays.toString(postfix));
+
+        int[] result = new int[nums.length];
+        for (int i = 0  ; i < nums.length; i++) {
+            if (i==0)
+                result[i] = postfix[i+1] * startPrefixPostfix;
+            else if(i==nums.length-1)
+                result[i] = prefix[i-1] * startPrefixPostfix;
+            else
+                result[i] = prefix[i-1] * postfix[i+1];
         }
         return result;
     }
