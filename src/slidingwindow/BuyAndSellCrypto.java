aecbd54340af17e56ba849cb28fcb08bde366964
printhelloword <slessudjono@gmail.com>
2024-06-07 17:36:56 +0700

    keep track of profits and return highest one

--- src/slidingwindow/BuyAndSellCrypto.java
+++ src/slidingwindow/BuyAndSellCrypto.java
@@ -6,12 +6,15 @@ public class BuyAndSellCrypto {
         System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
         System.out.println(maxProfit(new int[]{7,6,4,3,1}));
         System.out.println(maxProfit(new int[]{2,4,1}));
+        System.out.println(maxProfit(new int[]{3,2,6,5,0,3}));
     }
     
     public static int maxProfit(int[] prices) {
         int lowerLowIndex = 0;
         int higherHighIndex = 0;
+        int highestProfit=0;
         for (int i = 0; i < prices.length; i++) {
+            int profit;
             if (i==0) continue;
             if(prices[i] < prices[lowerLowIndex]){
                 if (i+1 != prices.length){
@@ -20,14 +23,16 @@ public class BuyAndSellCrypto {
             } 
             else if (prices[i] > prices[higherHighIndex])
                 higherHighIndex = i;
-        }
 
-        int result;
-        if (higherHighIndex == -1)
-            result = 0;
-        else 
-            result = prices[higherHighIndex] - prices[lowerLowIndex];
-
-        return result;
+            if (higherHighIndex == -1)
+                profit = 0;
+            else 
+                profit = prices[higherHighIndex] - prices[lowerLowIndex];
+            
+            if (profit>highestProfit){
+                highestProfit=profit;
+            }
+        }     
+        return highestProfit;
     }
 }
