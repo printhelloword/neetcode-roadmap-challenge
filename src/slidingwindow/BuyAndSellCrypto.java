4de4237b041c80327afc76ff702d20f24ae0d53e
printhelloword <slessudjono@gmail.com>
2024-06-07 14:33:39 +0700

    init sliding with init class of problem best time to buy and sell stock

---
+++ src/slidingwindow/BuyAndSellCrypto.java
@@ -0,0 +1,13 @@
+package slidingwindow;
+
+public class BuyAndSellCrypto {
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7,1,5,3,6,4}));
+        System.out.println(maxProfit(new int[]{7,6,4,3,1}));
+    }
+    
+    public static int maxProfit(int[] prices) {
+        return 1;
+    }
+}
