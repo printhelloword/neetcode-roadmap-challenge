9db74a03b9d82b3e5f52abada69492c144f2f2e4
printhelloword <slessudjono@gmail.com>
2024-06-07 14:27:33 +0700

    add solution to binary search

--- src/binarysearch/BinarySearch.java
+++ src/binarysearch/BinarySearch.java
@@ -7,7 +7,10 @@ public class BinarySearch {
     }
 
     public static int search(int[] nums, int target) {
-        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target)
+                return i;
+        }
         return -1;
     }
     
