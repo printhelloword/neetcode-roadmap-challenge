b24fc90d504c17810ee7ee45287a1dc6edca2c4e
printhelloword <slessudjono@gmail.com>
2024-06-07 14:40:52 +0700

    init linked list with init class for problem reverse a linked list

---
+++ src/linkedlist/ReverseALinkedList.java
@@ -0,0 +1,13 @@
+package linkedlist;
+
+public class ReverseALinkedList {
+    public static void main(String[] args) {
+        
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        
+        return new ListNode();
+    }
+    
+}
