5c9007ae45652806b63bdf633ec534ed24d73d69
printhelloword <slessudjono@gmail.com>
2024-06-07 16:21:04 +0700

    add  solution to when to buy and sell stock

--- src/slidingwindow/BuyAndSellCrypto.java
+++ src/slidingwindow/BuyAndSellCrypto.java
@@ -8,6 +8,24 @@ public class BuyAndSellCrypto {
     }
     
     public static int maxProfit(int[] prices) {
-        return 1;
+        int lowerLowIndex = 0;
+        int higherHighIndex = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (i==0) continue;
+            if(prices[i] < prices[lowerLowIndex]){
+                lowerLowIndex = i;
+                higherHighIndex = i;
+            } 
+            else if (prices[i] > prices[higherHighIndex])
+                higherHighIndex = i;
+        }
+
+        int result;
+        if (higherHighIndex == -1)
+            result = 0;
+        else 
+            result = prices[higherHighIndex] - prices[lowerLowIndex];
+
+        return result;
     }
 }
