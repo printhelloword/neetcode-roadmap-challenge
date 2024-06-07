4f1347a5bf50637aebf862b91bcaaa16b786ca32
printhelloword <slessudjono@gmail.com>
2024-06-07 13:52:33 +0700

    init binary search

---
+++ src/binarysearch/BinarySearch.java
@@ -0,0 +1,14 @@
+package binarysearch;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        System.out.println(search(new int[]{-1,0,2,4,6,8}, 6));
+        System.out.println(search(new int[]{-1,0,2,4,6,8}, 3));
+    }
+
+    public static int search(int[] nums, int target) {
+        
+        return -1;
+    }
+    
+}
